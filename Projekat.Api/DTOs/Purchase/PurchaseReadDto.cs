f4fdd5b68cf890b7462f1204cf6d0c1576ca4ec2
Lazar928 <68812501+Lazar928@users.noreply.github.com>
2026-02-10 17:39:49 +0100

    Update PurchaseReadDto.cs

--- Projekat.Api/DTOs/Purchase/PurchaseReadDto.cs
+++ Projekat.Api/DTOs/Purchase/PurchaseReadDto.cs
@@ -6,7 +6,7 @@ public class PurchaseReadDto
     public int VehicleId { get; set; }
     public string VehicleName { get; set; } = null!;
     public string BuyerUsername { get; set; } = null!;
-    public string OwnerUsername { get; set; } // 👈 NOVO
+    public string OwnerUsername { get; set; }
 
     public string BuyerEmail { get; set; }
     public DateTime CreatedAt { get; set; }
