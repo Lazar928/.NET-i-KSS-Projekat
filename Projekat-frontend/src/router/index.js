7096e0a14cb5bfa7b98a658b25a295e2336322fd
Lazar928 <68812501+Lazar928@users.noreply.github.com>
2026-02-10 17:49:35 +0100

    Update index.js

--- Projekat-frontend/src/router/index.js
+++ Projekat-frontend/src/router/index.js
@@ -27,23 +27,23 @@ const router = createRouter({
   routes
 })
 
-// 🔐 ROUTE GUARD
+// ROUTE GUARD
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('token')
 
-  // Ako je ulogovan i pokušava login ili register → prebaci na home
+  // Ako je ulogovan i pokusava login ili register onda prebaci na home
   if (token && (to.path === '/login' || to.path === '/register')) {
     next('/home')
     return
   }
 
-  // Ako NIJE ulogovan i ide na zaštićenu rutu → vrati na login
+  // Ako nije ulogovan i ide na zasticenu rutu onda vrati na login
   if (!token && to.path !== '/login' && to.path !== '/register') {
     next('/login')
     return
   }
 
-  // U svim ostalim slučajevima → pusti
+  // U svim ostalim slučajevima pusti
   next()
 })
 
