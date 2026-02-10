3e4f86422809548a46f3b4620f356dddac2d3f42
Lazar928 <68812501+Lazar928@users.noreply.github.com>
2026-02-10 19:03:17 +0100

    Update VehicleUpdateDto.cs

--- Projekat.Api/DTOs/Vehicle/VehicleUpdateDto.cs
+++ Projekat.Api/DTOs/Vehicle/VehicleUpdateDto.cs
@@ -7,6 +7,8 @@ namespace Projekat.Api.DTOs.Vehicle
         public int Year { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } //Nije ubaceno u funkciju, ali je ostavljeno za prosirenje
     }
 }
+
+//Ulazni DTO, sluzi za izmenu vozila i sadrzi samo podatke koji su dozvoljeni da se menjaju
