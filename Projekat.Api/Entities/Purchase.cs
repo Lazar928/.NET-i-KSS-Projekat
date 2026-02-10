3af185d07026b06e641a63f129fdfa286d071f91
Lazar928 <68812501+Lazar928@users.noreply.github.com>
2026-02-10 18:38:46 +0100

    Update Purchase.cs

--- Projekat.Api/Entities/Purchase.cs
+++ Projekat.Api/Entities/Purchase.cs
@@ -4,12 +4,12 @@ namespace Projekat.Api.Entities;
 
 public class Purchase
 {
-    public int Id { get; set; }
+    public int Id { get; set; } // pk
 
-    public int BuyerId { get; set; }
+    public int BuyerId { get; set; } //fk
     public User Buyer { get; set; } = null!;
 
-    public int VehicleId { get; set; }
+    public int VehicleId { get; set; } //fk
     public Vehicle Vehicle { get; set; } = null!;
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
