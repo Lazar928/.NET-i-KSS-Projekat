aea08039d97e8ef123fb1a21c05b8b24c6fe9186
Lazar928 <68812501+Lazar928@users.noreply.github.com>
2026-02-10 18:53:53 +0100

    Update RegisterDto.cs

--- Projekat.Api/DTOs/Auth/RegisterDto.cs
+++ Projekat.Api/DTOs/Auth/RegisterDto.cs
@@ -5,5 +5,8 @@ public class RegisterDto
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string Role { get; set; } // Owner | Buyer | Admin
+    public string Role { get; set; } // Owner | Buyer
 }
+
+//Frontend salje podatke za pravljenje novog korisnika
+// Kasnije smo u frontendu ogranicili da korisnik ne moze da izabere rolu admin pri registraciji
