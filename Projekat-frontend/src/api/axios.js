8b44b28149e7a6fa20df683a3636d62744f5a699
Lazar928 <68812501+Lazar928@users.noreply.github.com>
2026-02-11 15:16:52 +0100

    Update axios.js

--- Projekat-frontend/src/api/axios.js
+++ Projekat-frontend/src/api/axios.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from "axios"; // Axios biblioteka za HTTP zahteve
 
 const api = axios.create({
   baseURL: "http://localhost:5097/api", // backend projekta
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => { // Presretanje svakog requsta pre odlaska ka serveru
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = 'Bearer ' + token;
+    config.headers.Authorization = 'Bearer ' + token; // PRovera da li postoj JWT token i uzima ga i omogucava back-endu da ga procita
   }
   return config;
 });
